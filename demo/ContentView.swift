1638bb2bbe0e41dcf6f415e2becf384d682e6d0e
uwu <uwu@upc.edu.pe>
2023-06-22 13:32:52 -0500

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by Usuario de proyectos on 22/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Usuario de proyectos on 22/6/23.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
